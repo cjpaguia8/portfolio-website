7cfd2c604bc477e3ba9778250b91d6f7a58954c2
cpaguia <cpaguia@hawk.iit.edu>
2024-11-24 17:05:11 -0600

    threejscanvas

--- src/Components/ThreeJSCanvas/ThreeJSCanvas.jsx
+++ src/Components/ThreeJSCanvas/ThreeJSCanvas.jsx
@@ -7,9 +7,13 @@ const ThreeJSCanvas = () => {
 
   useEffect(() => {
     // Create the scene, camera, and renderer
+    var objColor = new THREE.Color().setHex(0x00ff00);
+    var bgColor = new THREE.Color().setHex(0x000000);
+
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      40,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
@@ -21,11 +25,15 @@ const ThreeJSCanvas = () => {
     // Append renderer's canvas to the container
     canvasContainerRef.current.appendChild(renderer.domElement);
 
+    // Set background color
+    scene.background = new THREE.Color(bgColor); 
+
     // Add a cube to the scene
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // const geometry = new THREE.TorusGeometry
+    const material = new THREE.MeshBasicMaterial({ color: objColor });
+    const object = new THREE.Mesh(geometry, material);
+    scene.add(object);
 
     // Set the camera position
     camera.position.z = 5;
@@ -33,8 +41,8 @@ const ThreeJSCanvas = () => {
     // Animation loop
     const animate = () => {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      object.rotation.x += 0.01;
+      object.rotation.y += 0.01;
       renderer.render(scene, camera);
     };
 
